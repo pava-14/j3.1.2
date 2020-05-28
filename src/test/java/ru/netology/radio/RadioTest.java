bf3579557df5f4f5b4619e7fd737ddaf6b3dc9e4
Alexander Pavlenko <pava@bk.ru>
2020-05-28 18:24:02 +0700

    Test Class Created

---
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -0,0 +1,28 @@
+package ru.netology.radio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    void setCurrentRadioStation() {
+    }
+
+    @Test
+    void increaseCurrentVolume() {
+    }
+
+    @Test
+    void decreaseCurrentVolume() {
+    }
+
+    @Test
+    void nextRadioStation() {
+    }
+
+    @Test
+    void prevRadioStation() {
+    }
+}
