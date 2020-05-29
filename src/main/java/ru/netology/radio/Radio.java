2057aba51de0208cf24390aa5bda4f0bb7835f3a
Alexander Pavlenko <pava@bk.ru>
2020-05-29 10:46:00 +0700

    Volume Tests Created

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -3,11 +3,10 @@ package ru.netology.radio;
 public class Radio {
     private int currentStation;
     private int currentVolume;
-    private int minStation;
-    private int maxStation;
-    private int minVolume;
-    private int maxVolume;
-
+    private final int minStation = 0;
+    private final int maxStation = 9;
+    private final int minVolume = 0;
+    private final int maxVolume = 10;
 
     public int getCurrentStation() {
         return currentStation;
@@ -43,32 +42,16 @@ public class Radio {
         return minStation;
     }
 
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-
     public int getMaxStation() {
         return maxStation;
-    }
-
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
     }
 
     public int getMinVolume() {
         return minVolume;
     }
 
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-
     public int getMaxVolume() {
         return maxVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
     }
 
     public void increaseCurrentVolume() {
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -8,24 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
-    void initRadio(Radio radio) {
-        radio.setMaxVolume(10);
-        radio.setMinVolume(0);
-        radio.setMaxStation(9);
-        radio.setMinStation(0);
+    void selectRandomStation(Radio radio) {
+
+        Random random = new Random();
+        // Выбираем случайную станцию из диапазона 0..10
+        radio.setCurrentStation(random.nextInt(10));
     }
 
     @Test
     void shouldSetCurrentStation() {
 
         Radio radio = new Radio();
-        initRadio(radio);
 
         Random random = new Random();
 
         // Клиент должен иметь возможность выставлять номер
         // радиостанции с цифрового пульта (вводя числа 0 - 9)
         int expected = random.nextInt(10);
+
         radio.setCurrentStation(expected);
 
         assertEquals(expected, radio.getCurrentStation());
@@ -41,10 +41,9 @@ class RadioTest {
     void shouldIncreaseCurrentVolume() {
 
         Radio radio = new Radio();
-        initRadio(radio);
 
         // Нажимаем кнопку увеличения громкости
-        for (int i = 0; i < 20 ; i++) {
+        for (int i = 0; i < 20; i++) {
             radio.increaseCurrentVolume();
         }
 
@@ -55,10 +54,9 @@ class RadioTest {
     void shouldDecreaseCurrentVolume() {
 
         Radio radio = new Radio();
-        initRadio(radio);
 
-        // Нажимаем кнопку уменьшения громкости
-        for (int i = 0; i < 20 ; i++) {
+        // Нажимаем 20 раз кнопку уменьшения громкости
+        for (int i = 0; i < 20; i++) {
             radio.decreaseCurrentVolume();
         }
 
@@ -67,17 +65,26 @@ class RadioTest {
 
     @Test
     void shouldNextRadioStation() {
+
         Radio radio = new Radio();
-        initRadio(radio);
-        int expected = 2;
+
+        selectRandomStation(radio);
+
+        // Количество начатий на кнопу next
+        int countClick = 13;
+
+        int countStation = radio.getMaxStation() - radio.getMinStation();
+        countStation += 1; //zero based
+
+        int expected = countClick % countStation + radio.getCurrentStation();
 
         // Нажимаем кнопку next станция
-        for (int i = 0; i < 12 ; i++) {
+        for (int i = 0; i < countClick; i++) {
             radio.nextStation();
             System.out.println(radio.getCurrentStation());
         }
 
-        assertEquals(expected , radio.getCurrentStation());
+        assertEquals(expected, radio.getCurrentStation());
 
     }
 
